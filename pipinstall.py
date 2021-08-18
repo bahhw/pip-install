7b404d9574e59dc922f31fd7827e50240444bfee
bahhh <88990516+bahhw@users.noreply.github.com>
2021-08-18 00:36:42 -0300

    Create pipinstall.py
    Follow the steps correctly and enjoy!

---
+++ pipinstall.py
@@ -0,0 +1,21 @@
+# pip install and PyPi
+
+# PyPi is: The Python Package Index or PyPI is the official software repository
+# for third-party applications in the Python programming language.
+# Python developers intend it to be a comprehensive catalog
+# of all Python packages written in open source PyPI is analogous
+# to CPAN for Perl, or PEAR for PHP. SOURCE = Wikipedia.
+
+# pip install requests
+# pip install colorama
+
+from colorama import init
+init()
+from colorama import Fore
+print(Fore.RED + "This is RED text")
+print(Fore.GREEN + "This is GREEN text")
+print(Fore.YELLOW + "This is YELLOW text")
+
+# Perform a Google search to work with EXCEL files
+
+# pip install openpyxl
